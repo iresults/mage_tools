43b849869bc6bb308d461065bc335fdead0c63ec
Daniel Corn <daniel@iresults.li>
2017-06-01 16:08:46 +0200

    Added command to dry run (simulate) a patch

--- lib.sh
+++ lib.sh
@@ -2,6 +2,24 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
+function lib::has_argument() {
+    if [[ "$#" -lt "1" ]]; then
+        lib::print_error "Missing argument 1 (search)";
+    fi
+    if [[ "$#" -lt "2" ]]; then
+        lib::print_error "At least 2 arguments must be given";
+    fi
+    local search="$1";
+    shift;
+    for var in "$@"; do
+        if [[ "$var" == "$search" ]]; then
+            echo "true";
+            return;
+        fi
+    done
+    echo "false";
+}
+
 function _print_colored() {
     test -t 1 && tput setaf $2;
     echo "$1";
@@ -19,8 +37,13 @@ function lib::print_warning() {
     >&2 echo "[WARNING]: $1";
     test -t 1 && tput setaf 0;
 }
+
 function lib::print_head() {
     _print_colored "[TASK]: $1" 2;
+}
+
+function lib::print_success() {
+    _print_colored "$1" 2;
 }
 
 function lib::print_demand() {
